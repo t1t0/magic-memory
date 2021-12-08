17c31bcb4fa8d2c1d54eb5672267f547068b4a05
Viktor Rodriguez <viktor.a.rodriguez.o@gmail.com>
2021-12-08 14:47:57 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -25,6 +25,8 @@ function App() {
       .sort(() => Math.random() - 0.5)
       .map((card) => ({ ...card, id: Math.random() }))
 
+    setChoiceOne(null)
+    setChoiceTwo(null)
     setCards(shuffleCards)
     setTurns(0)
   }
@@ -67,6 +69,10 @@ function App() {
     setDisabled(false)
   }
 
+  useEffect(() => {
+    shuffleCards()
+  }, [])
+
   return (
     <div className="App">
       <h1>Magic match</h1>
@@ -83,6 +89,7 @@ function App() {
           />
         ))}
       </div>
+      <p>Turns: {turns}</p>
     </div>
   );
 }
